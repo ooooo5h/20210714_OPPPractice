b4bab8508f487525eab72d5712ae44a345009da7
전은형 <ooooo5h91@gmail.com>
2021-07-14 21:54:58 +0900

    책 - 정보 출력 기능

--- src/codes/Book.java
+++ src/codes/Book.java
@@ -8,5 +8,14 @@ public class Book {
 	
 //	Q1. 빌려간 사람이 누군지 만들자
 	User rentUser;
+	
+//	Q2. 책의 정보 출력 기능
+	void printBookInfo() {
+		System.out.println("==============");
+		System.out.println("책 제목 : " + title);
+		System.out.println("대여료 : " + rentFee);
+		System.out.println("연령제한 : " + limitAge);
+		System.out.println("==============");
+	}
 
 }
